4cb00b8b389c4eb9ba1975a5ce58a26f3da6964b
Nicorici <nickorici@yahoo.com>
2019-01-28 13:32:21 +0200

    Add project files.

---
+++ DictionaryFacts/DictionaryFacts.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Dictionary.Tests
+{
+    public class DictioanaryFacts
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
