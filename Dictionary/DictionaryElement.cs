eeef726dca19d2b7c53621cd56cd577ae753a8aa
Nicorici <nickorici@yahoo.com>
2019-01-29 14:31:08 +0200

    Created a class representing a typical element in a Dictionary collection

---
+++ Dictionary/DictionaryElement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    public class DictionaryElement<TKey, TValue>
+    {
+        public TKey Key { get; set; }
+        public TValue Value { get; set; }
+        public int Next { get; set; } = -1;
+
+        public DictionaryElement(TKey key, TValue value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+}
